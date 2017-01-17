9ba099016ae32f33cacc7b12b11ac006339417f7
sweetsbeats <the.patton@yahoo.ca>
2017-01-17 13:52:00 -0800

    Created main.cpp
    -ver0.0.2

---
+++ a.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+//math.h forward delcarations
+int add(int x, int y);
+int square(int x);
+
+
+int main() {
+
+  bool playing = true; // Bool for the game loop
+
+  while (playing == true) {  //Main game loop
+    int menuInput;
+
+    std::cout << "Enter 1 to play, 0 to exit\n";
+    std::cin >> menuInput;
+
+    if (menuInput == 1) {
+      int answer;
+      int guessNumber;
+      bool gotanswer = false;
+
+      std::cout << "To start, what is your favourite number?\n";
+      int favouriteNumber;
+      std::cin >> favouriteNumber;
+
+      answer = square(favouriteNumber);
+
+      std::cout << "I'm thinking of a number between 1 and "<< add(answer, favouriteNumber) << ", GUESS\n";
+      std::cin >> guessNumber;
+     
+      if (guessNumber == answer) {            //This extra check fixes a bug where the right
+        std::cout << "You win!\n";            //answer wouldn't get taken the first time
+      } else if (guessNumber != answer) {
+          while (!gotanswer) {
+            
+            if (guessNumber > answer)
+            {
+              std::cout << "think smaller, ";
+            }
+            else 
+            {
+              std::cout << "think bigger, ";
+            }
+            
+            std::cout << "Try again!\n";
+            std::cin >> guessNumber;            
+          if (guessNumber == answer) {
+            std::cout << "You win!\n";
+            break;
+        } else {
+            continue;
+        }
+       }
+      }
+
+
+
+    } else if (menuInput == 0) {
+      playing = false;
+
+    } else {
+      std::cout << "not a valid command!\n";
+    }
+
+  }
+
+  return 0;
+}
+
+//math.cpp delcarations
+
+int add(int x, int y) {
+return x + y;
+}
+
+int square(int x) {
+return x * x;
+}

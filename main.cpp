b9366e9dfe7f05b59632924b276b141c31fff1d3
sweetsbeats <the.patton@yahoo.ca>
2017-01-25 10:43:14 -0800

    std::strings!
    Added input via strings

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "math.h"
 
 int main() {
@@ -6,43 +7,55 @@ int main() {
   bool playing = true; // Bool for the game loop
 
   while (playing == true) {  //Main game loop
-    int menuInput;
+    std::string menuInput;
 
-    std::cout << "Enter 1 to play, 0 to exit\n";
+    std::cout << "Type 'play' to play, or 'exit' to exit!\n";
     std::cin >> menuInput;
 
-    if (menuInput == 1) {
-      int answer;
-      int guessNumber;
+    if (menuInput == "play") {
+      float answer;
+      float guessNumber;
       bool gotanswer = false;
 
       std::cout << "To start, what is your favourite number?\n";
-      int favouriteNumber;
+      float favouriteNumber;
       std::cin >> favouriteNumber;
+
 
       answer = square(favouriteNumber);
 
-      std::cout << "I'm thinking of a number between 1 and "<< add(answer, favouriteNumber) << ", GUESS\n";
+      std::cout << "I'm thinking of a number between 1 and "<< add(answer, absValue(favouriteNumber) * 2) << ", GUESS\n";
       std::cin >> guessNumber;
-     
+
       if (guessNumber == answer) {            //This extra check fixes a bug where the right
         std::cout << "You win!\n";            //answer wouldn't get taken the first time
       } else if (guessNumber != answer) {
           while (!gotanswer) {
+
+            if (guessNumber > answer)
+            {
+              std::cout << "think smaller, ";
+            }
+            else
+            {
+              std::cout << "think bigger, ";
+            }
+
             std::cout << "Try again!\n";
-            std::cin >> guessNumber;            
+            std::cin >> guessNumber;
           if (guessNumber == answer) {
             std::cout << "You win!\n";
             break;
         } else {
             continue;
         }
-       }
       }
 
+    }else if (menuInput == "fun") {
+      
+    }
 
-
-    } else if (menuInput == 0) {
+    } else if (menuInput == "exit") {
       playing = false;
 
     } else {
